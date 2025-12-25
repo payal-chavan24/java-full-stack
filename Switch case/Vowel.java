a40a8c7b9676d19ee164e0c510298aef71713062
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-25 23:29:34 +0530

    Switch case only
    Implemented a switch statement to determine if a character is a vowel or consonant.

---
+++ Switch case/Vowel.java
@@ -0,0 +1,47 @@
+package switchcase;
+
+// Take a single character input and check whether it is a vowel or consonant using a switch statement.
+public class Vowel {
+
+	public static void main(String[] args) {
+		char letter = 'Z';
+		switch (letter) {
+		case 'A':
+			System.out.println("Vowel");
+			break;
+		case 'E':
+			System.out.println("Vowel");
+			break;
+		case 'I':
+			System.out.println("Vowel");
+			break;
+		case 'O':
+			System.out.println("Vowel");
+			break;
+		case 'U':
+			System.out.println("Vowel");
+			break;
+		case 'a':
+			System.out.println("Vowel");
+			break;
+		case 'e':
+			System.out.println("Vowel");
+			break;
+		case 'i':
+			System.out.println("Vowel");
+			break;
+		case 'o':
+			System.out.println("Vowel");
+			break;
+		case 'u':
+			System.out.println("Vowel");
+			break;
+		default: {
+			System.out.println("Consonant");
+		}
+
+		}
+
+	}
+
+}

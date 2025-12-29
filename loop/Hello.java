b63a97101b27bb62c0e2c3d50e54c2c76390272f
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-29 08:25:21 +0530

    loop only
    Implement a loop to print 'Hello' 10 times.

---
+++ loop/Hello.java
@@ -0,0 +1,14 @@
+package loops; //Print Hello 10 times.
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			System.out.println("Hello " + "= " + i);
+			i++;
+		} while (i <= 10);
+
+	}
+
+}

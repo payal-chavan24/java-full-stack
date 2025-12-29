24595b4889607bc72bf3a85355542b375e9c2383
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-29 08:36:31 +0530

    loop only

---
+++ loop/Table2.java
@@ -0,0 +1,13 @@
+package loops;//print the table of any number using loop(n=7)
+
+public class Table2{
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10){
+			System.out.println("7x"+i+"="+i*7);
+			i++;
+		}
+	}
+
+}

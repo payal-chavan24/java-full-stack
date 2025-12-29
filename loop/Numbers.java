45646a83f1d3136547636f661bc6ec490eb5e7d0
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-29 08:30:53 +0530

    loop only
    This class prints numbers from 10 to 1 in descending order.

---
+++ loop/Numbers.java
@@ -0,0 +1,11 @@
+package loops;     //Print numbers from 10 to 1.
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		for( int i=10;i>=1;i--){
+			System.out.println(i);
+		}
+	}
+
+}

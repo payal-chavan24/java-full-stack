2fce2c907ef6a5619f0d4f5985abaaa905698638
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-29 23:10:16 +0530

    loop only

---
+++ Evennumberfor.java
@@ -0,0 +1,15 @@
+package loops;//print the even number between 1 to 200 using for loop
+
+public class Loop4 {
+
+	public static void main(String[] args) {
+		for( int i=1;i<=200;i++){
+			if(i%2==0){
+				System.out.println(i);
+			}
+			
+		}
+		
+	}
+
+}

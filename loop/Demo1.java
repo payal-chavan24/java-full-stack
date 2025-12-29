06b72d3fbc0ea5adfef8357fa7de5d8d3b3ebf47
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-29 08:20:42 +0530

    loop only
    Implement a loop to print numbers from 1 to 10.

---
+++ loop/Demo1.java
@@ -0,0 +1,13 @@
+package loops;
+
+//Print numbers from 1 to 10 using a loop.
+public class Demo1 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}

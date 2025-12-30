50b541a9e361c71868c13d3a3f263ce97d925d67
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2025-12-30 23:24:42 +0530

    BreakContinue

---
+++ BreakContinue/Number1.java
@@ -0,0 +1,14 @@
+//print the number from 1 to 50 but stop when number becomes 25 
+public class Number1 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 50; i++) {
+			if (i == 26) {
+				break;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}

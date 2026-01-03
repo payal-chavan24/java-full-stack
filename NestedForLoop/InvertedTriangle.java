4932fb28d94d3e3f2c200d0b802488344a49c379
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-03 22:54:20 +0530

    nested Loop only

---
+++ NestedForLoop/InvertedTriangle.java
@@ -0,0 +1,23 @@
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+
+
+
+public class InvertedTriangle {
+
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--) {//row decreasing so 5...to 1
+			for(int j=1;j<=i;j++) {//coulmn 
+			System.out.print("* ");	
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
+
+		

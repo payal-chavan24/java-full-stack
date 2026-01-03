e0002c5137c56ecf0324356cbe751a4b8916fa18
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-03 22:57:43 +0530

    nested Loop only

---
+++ NestedForLoop/SquareStar1.java
@@ -0,0 +1,18 @@
+//* * * * *
+//* * * * *
+//* * * * *
+//* * * * *
+//* * * * *
+
+public class SquareStar1 {
+
+	public static void main(String[] args) {
+		for (int i=1;i<=5;i++) {//rows
+			for( int j=1;j<=5;j++) {//column
+			System.out.print("* ");	//print star in one line
+			}
+			System.out.println(" ");//changes line
+		}
+	}
+
+}

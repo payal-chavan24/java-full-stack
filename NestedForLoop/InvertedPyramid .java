79e3cc3ab24f792079f641dd9c396eb6e3d7e108
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-03 22:53:16 +0530

    nested Loop only

---
+++ NestedForLoop/InvertedPyramid .java
@@ -0,0 +1,23 @@
+//* * * * *
+// * * * *
+//  * * *
+//   * *
+//    *
+
+public class InvertedPyramid {
+
+	public static void main(String[] args) {
+	int n=5;
+	for(int i=1;i<=n;i++) {
+		for(int s=1;s<=i-1;s++ ) {
+			System.out.print(" ");
+		}
+		for(int j=1;j<n-i+1;j++){
+			System.out.print("* ");
+		}
+		System.out.println("");
+	}
+
+	}
+
+}

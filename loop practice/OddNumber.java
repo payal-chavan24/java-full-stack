b91b1b28cd2b7930c4606c376256a5d2b3e20fa8
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-04 23:40:44 +0530

    loop only

---
+++ loop practice/OddNumber.java
@@ -0,0 +1,31 @@
+//Print all odd numbers between 1 and 50.
+public class OddNumber {
+	public static void main(String[] args) {
+		//for loop
+		System.out.println("for loop");
+		for(int i=1;i<=50;i++) {
+		if(i%2==1) {
+		System.out.println(i);	
+		}	
+		}
+		//while loop
+		System.out.println("while loop");
+		int i=1;
+		while(i<=50) {
+		if(i%2==1) {
+			System.out.println(i);
+		}
+		i++;
+		}
+		//do while loop 
+		System.out.println("do- while  loop");
+		int j=1;
+		do {
+			if(j%2==1) {
+				System.out.println(j);
+			}
+			j++;
+		}while(j<=50);
+	}
+
+}

27befa507412d2fdb3678f013ad0426ee9832302
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-04 23:38:54 +0530

    loop only

---
+++ loop practice/Number2.java
@@ -0,0 +1,22 @@
+//Print numbers from 10 to 1.
+public class Number2 {
+ public static void main (String args[]) {
+	//for loop
+	for(int i=10;i>=1;i--) {
+		System.out.println(i);
+	}
+	//while loop
+	int i=10;
+	while(i<=1) {
+		System.out.println(i);
+		i--;
+	}
+	// do-while loop
+	int j=10;
+	do {
+		System.out.println(j);
+		j--;
+	}while(j<=1);
+}
+}
+

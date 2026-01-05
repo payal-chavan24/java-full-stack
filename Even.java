497d3e9444e886125a166f96c343fea19452cc95
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-05 14:02:42 +0530

    Even.java

---
+++ Even.java
@@ -0,0 +1,13 @@
+//Print all even numbers between 1 to 200 using for loop.
+public class Even {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 200; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+
+		}
+
+	}
+
+}

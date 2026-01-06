85dc9eae4711c8524691287d0a9d9adc84c0e8e9
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:35:32 +0530

    simple program only

---
+++ loop practice/Simple program/AsciiValue.java
@@ -0,0 +1,10 @@
+//ASCII value of given character
+public class AsciiValue {
+	public static void main(String[] args) {
+		char uppercase = 'a';
+		int ascii = uppercase;
+		System.out.println("ascii value of " + uppercase + " " + ascii);
+
+	}
+
+}

b94c73b7a9fc4e6948512daba422609473cd9905
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:41:46 +0530

    simple program only

---
+++ loop practice/Simple program/MultiplicationTable.java
@@ -0,0 +1,12 @@
+// multipplication table of 7
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("7x" + i + "=" + 7 * i);
+
+		}
+
+	}
+
+}

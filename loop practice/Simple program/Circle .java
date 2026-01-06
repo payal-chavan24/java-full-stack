f423dfdac3824a37217d69fec4473e47cf0a22f0
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:37:19 +0530

    simple program only

---
+++ loop practice/Simple program/Circle .java
@@ -0,0 +1,11 @@
+//Area of circle
+public class Circle {
+	public static void main(String[] args) {
+		double r = 3;
+		double p = 3.142;
+		double area = p * r * r;
+		System.out.println("area of circle=" + area);
+
+	}
+
+}

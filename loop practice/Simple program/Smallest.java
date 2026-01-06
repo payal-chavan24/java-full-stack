4203e26faf543f8bb8baf33afdf3c76dd090deb9
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:44:27 +0530

    simple program only

---
+++ loop practice/Simple program/Smallest.java
@@ -0,0 +1,14 @@
+//write a program to accept two number and find smallest number
+public class Smallest {
+	public static void main(String[] args) {
+		int num1 = 7, num2 = 8;
+		if (num1 < num2) {
+			System.out.println("num1 is smallest");
+		} else if (num1 > num2) {
+			System.out.println("num2 is smallest");
+		} else {
+			System.out.println("both are equal");
+		}
+	}
+
+}

8fdc9b5ebbba9b6e41bc44f164b986cdfc5c152c
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:42:35 +0530

    simple program only

---
+++ loop practice/Simple program/Number .java
@@ -0,0 +1,15 @@
+//write a program to accept a number and find it zero ,positive,negative
+public class Number {
+	public static void main(String[] args) {
+		int num = 34;
+		if (num > 0) {
+			System.out.println(num + " is a positive number");
+		} else if (num < 0) {
+			System.out.println(num + " is a negitive number");
+		} else {
+			System.out.println(num + " is zero");
+		}
+
+	}
+
+}

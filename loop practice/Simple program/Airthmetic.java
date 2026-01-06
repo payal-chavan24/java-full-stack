2ec046c65ae2d79b54c0eb65ca20c431f0da2e1b
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:34:32 +0530

    simple program only

---
+++ loop practice/Simple program/Airthmetic.java
@@ -0,0 +1,16 @@
+//airthmetic operator
+public class Airthmetic {
+	public static void main(String[] args) {
+		int a = 6, b = 8;
+		int sum = a + b;
+		int Subtraction = b - a;
+		int multiplication = a * b;
+		int division = a / b;
+		System.out.println(sum);
+		System.out.println(Subtraction);
+		System.out.println(multiplication);
+		System.out.println(division);
+
+	}
+
+}

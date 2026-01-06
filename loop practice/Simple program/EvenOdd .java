379a63565c04e3ceedefad868025824bbee9fc37
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:39:10 +0530

    simple program only

---
+++ loop practice/Simple program/EvenOdd .java
@@ -0,0 +1,13 @@
+//Write a program to accept a number and find it is even or odd
+public class EvenOdd {
+	public static void main(String[] args) {
+		int num = 45;
+		if (num % 2 == 0) {
+			System.out.println(num + " is a even number");
+		} else {
+			System.out.println(num + " is a odd number");
+		}
+
+	}
+
+}

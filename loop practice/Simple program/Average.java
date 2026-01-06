9f4c863ec04227f721f74c755d3e32e1de1f4b79
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:36:17 +0530

    simple program only

---
+++ loop practice/Simple program/Average.java
@@ -0,0 +1,10 @@
+//find average of three number
+public class Average {
+	public static void main(String[] args) {
+		int a = 3, b = 7, c = 9;
+		int sum = a + b + c;
+		int average = sum / 3;
+		System.out.println(average);
+	}
+
+}

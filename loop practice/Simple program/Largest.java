f98096f4458f2eff3174e1c3b60c766852059d6b
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-06 22:39:58 +0530

    simple program only

---
+++ loop practice/Simple program/Largest.java
@@ -0,0 +1,13 @@
+//write a program to accept two number and find largest number
+public class Largest {
+	public static void main(String args[]) {
+		int num1 = 54, num2 = 78;
+		if (num1 > num2) {
+			System.out.println("num1 is largest");
+		} else if (num1 < num2) {
+			System.out.println("num2 is largest");
+		} else {
+			System.out.println("both are equal");
+		}
+	}
+}

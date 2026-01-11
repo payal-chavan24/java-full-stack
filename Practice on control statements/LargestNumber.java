8e0cc2389cabc011706323b2a8a4a9e395ce5bf6
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-11 22:20:36 +0530

    Control Statement

---
+++ Practice on control statements/LargestNumber.java
@@ -0,0 +1,24 @@
+
+//write a program to accept three number and find largest number
+import java.util.Scanner;
+
+public class LargestNumber {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int a = sc.nextInt();
+		System.out.println("Enter b number");
+		int b = sc.nextInt();
+		System.out.println("Enter c number");
+		int c = sc.nextInt();
+		if (a > b) {
+			System.out.println("a is largest");
+		} else if (b > c) {
+			System.out.println("b is largest");
+		} else {
+			System.out.println("c  is largest");
+		}
+
+	}
+
+}

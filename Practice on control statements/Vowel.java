685ef3b7af9c2ed53e62097a1c715ba8eab8f72e
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-12 06:47:21 +0530

    Control Statement

---
+++ Practice on control statements/Vowel.java
@@ -0,0 +1,17 @@
+//write a java Accept character and whether it is vowel or consonant
+import java.util.Scanner;
+public class Vowel {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a given character");
+		char ch = sc.next().charAt(0);
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+				|| ch == 'O' || ch == 'U') {
+			System.out.println("Vowel");
+		} else {
+			System.out.println("consonant");
+		}
+		sc.close();
+
+	}
+}

2f7c57890867e9000e3de6af3665905f4f0a2edd
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-12 06:31:13 +0530

    Control Statement

---
+++ Practice on control statements/Weekend.java
@@ -0,0 +1,32 @@
+//weekend
+public class Weekend {
+	public static void main(String[] args) {
+		int weekend = 2;
+		switch (weekend) {
+		case 1:
+			System.out.println("sunday");
+			break;
+		case 2:
+			System.out.println("monday");
+			break;
+		case 3:
+			System.out.println("tuesday");
+			break;
+		case 4:
+			System.out.println("wednesday");
+			break;
+		case 5:
+			System.out.println("thursday");
+			break;
+		case 6:
+			System.out.println("friday");
+			break;
+		case 7:
+			System.out.println("saturday");
+			break;
+		default:
+			System.out.println("enter invalid number");
+
+		}
+	}
+}

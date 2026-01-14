7a056d884c37f1dfe31515020f30e90e1597bc5c
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-14 18:47:28 +0530

    logical question

--- logical question/PrimeNumber2.java
+++ logical question/PrimeNumber2.java
@@ -11,5 +11,30 @@ public class PrimeNumber2 {
 				System.out.println(i);
 			}
 		}
+//using Scanner class
+		import java.util.Scanner;
+public class Practice4 {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a range");
+		int num=sc.nextInt();
+		for (int i = 1; i <=num; i++) {
+			int count = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					count++;
+				}
+			}
+			if (count == 2) {
+				System.out.println(i);
+			}
+		}
+		sc.close();
+	}
+}
+
+
+
+		
 	}
 }

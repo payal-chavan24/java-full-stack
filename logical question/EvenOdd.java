17146cded4934471bf0895fefc6195d81a042d01
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-14 19:18:09 +0530

    logical question

---
+++ logical question/EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class EvenOdd {
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a limit ...!");
+		int num=sc.nextInt();
+		for(int i=1;i<=num;i++) {
+			if(i%2==0) {
+				System.out.println(i+" is even number");
+			}else {
+				System.out.println(i+" is odd number");
+			}
+			
+			
+		}
+		sc.close();
+	}
+}

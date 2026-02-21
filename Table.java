0a84d1fda1cbad5003de1cdf092b727c7532291f
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-02-21 22:09:07 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,13 @@
+package loops;//print the table of 5 using loop
+
+public class Table {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while (i <= 10) {
+			System.out.println("5x" + i + "=" + i * 5);
+			i++;
+		}
+	}
+
+}
